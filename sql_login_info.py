24279ad6a378b20a6652105fdf92eb7c92ead8cb
stan870224 <103738164+stan870224@users.noreply.github.com>
2024-11-19 22:46:10 +0800

    Update sql_login_info.py

--- sql_login_info.py
+++ sql_login_info.py
@@ -1,7 +1,7 @@
-def mssql_login_info() :
+def mssql_login_info():
     server = 'localhost'
     database = 'Finance_data'
     username = 'tony'
     password = '12345678'
-    
-    return server,database,username,password
+
+    return server, database, username, password
