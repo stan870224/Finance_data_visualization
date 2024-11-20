c31e75ac81769a914d633ffd56730d29dd744af9
tony <tony.wu@softbi.com>
2024-11-20 13:05:18 +0800

    20241120

--- test_sql.py
+++ test_sql.py
@@ -1,14 +1,14 @@
 import yfinance as yf
 from datetime import datetime, timedelta
-import Finance_Catch_10y as fi
 import pandas as pd
+from finance_catch_10y import fetch_historical_data
 
 
 end_date = datetime.today().strftime('%Y-%m-%d')
 start_date = (datetime.today() - timedelta(days=365*10)).strftime('%Y-%m-%d')
 stock_code = ['1210.TW','2330.TW']
 
-historical_data = fi.fetch_historical_data(stock_code, start_date, end_date)
+historical_data = fetch_historical_data(stock_code, start_date, end_date)
 
 df = pd.DataFrame(historical_data)
 
