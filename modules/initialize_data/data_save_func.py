e251efde68be947631452352d7c88b337e218a69
stan870224 <103738164+stan870224@users.noreply.github.com>
2024-11-23 22:34:16 +0800

    20241123
    程式跟資料位置超級大洗牌 以readme.md的架構規劃存放程式

---
+++ modules/initialize_data/data_save_func.py
@@ -0,0 +1,10 @@
+import os
+
+def data_save():
+    data_dir = 'D:' + os.path.sep + 'AI_course' + os.path.sep + 'Finance_data_visualization' + os.path.sep + 'data' + os.path.sep + 'row_data'
+    file_path = 'D:' + os.path.sep + 'AI_course' + os.path.sep + 'Finance_data_visualization' + os.path.sep + 'data' + os.path.sep + 'processed_data' + os.path.sep+'top_market_cap_by_sector'
+    output_file = os.path.join(file_path, 'top_market_cap_by_sector.csv')
+    input_file = os.path.join(file_path, 'top_market_cap_by_sector.csv')
+    table_name = 'Stock_Historical_Data'
+    
+    return data_dir, file_path, output_file, input_file, table_name
